fixed normalization

src/models/decoder.py
@@ -56,8 +56,10 @@ class Decoder(nn.Module):
             blocks.append(nn.BatchNorm2d(block_out_ch))
         elif norm=="group":
             blocks.append(nn.GroupNorm(num_groups=norm_num_groups, num_channels=block_out_ch))
-        elif norm=="layer":
+        elif norm=="instance":
             blocks.append(nn.GroupNorm(num_groups=block_out_ch, num_channels=block_out_ch))
+        elif norm=="layer":
+            blocks.append(nn.GroupNorm(num_groups=1, num_channels=block_out_ch))
         blocks.append(nn.Conv2d(block_out_ch, out_channels, kernel_size=3, stride=1, padding=1))
         self.blocks = nn.ModuleList(blocks)
 

src/models/encoder.py
@@ -57,8 +57,10 @@ class Encoder(nn.Module):
             blocks.append(nn.BatchNorm2d(output_channel))
         elif norm=="group":
             blocks.append(nn.GroupNorm(num_groups=norm_num_groups, num_channels=output_channel))
-        elif norm=="layer":
+        elif norm=="instance":
             blocks.append(nn.GroupNorm(num_groups=output_channel, num_channels=output_channel))
+        elif norm=="layer":
+            blocks.append(nn.GroupNorm(num_groups=1, num_channels=output_channel))
         blocks.append(nn.Conv2d(output_channel, out_channels, 3,1,1))
         self.blocks = nn.ModuleList(blocks)
 

src/models/layers/blocks.py
@@ -17,9 +17,12 @@ class customResBlock(nn.Module):
         elif norm == "group":
             self.norm1 = nn.GroupNorm(num_groups=norm_num_groups, num_channels=in_channels)
             self.norm2 = nn.GroupNorm(num_groups=norm_num_groups, num_channels=out_channels)
-        elif norm == "layer":
+        elif norm == "instance":
             self.norm1 = nn.GroupNorm(num_groups=in_channels, num_channels=in_channels)
             self.norm2 = nn.GroupNorm(num_groups=out_channels, num_channels=out_channels)
+        elif norm == "layer":
+            self.norm1 = nn.GroupNorm(num_groups=1, num_channels=in_channels)
+            self.norm2 = nn.GroupNorm(num_groups=1, num_channels=out_channels)
         else:
             self.norm1=nn.Identity()
             self.norm2=nn.Identity()